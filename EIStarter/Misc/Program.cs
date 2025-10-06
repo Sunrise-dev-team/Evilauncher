f2e10effbcc556ab137cc98419bba143d6201abd
Atom-FG <Atom-FG@users.noreply.github.com>
2025-10-06 16:13:30 +0300

    Program.cs: Mutex for only one instance of starter

--- EIStarter/Misc/Program.cs
+++ EIStarter/Misc/Program.cs
@@ -3,29 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.Runtime.InteropServices;
 
 namespace EIStarter
 {
     internal static class Program
     {
+        private static Mutex mutex;
+        const string uniqueMutexName = "EIStarterSharp";
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // TODO: activate exist copy of program or just close when game.exe running!
+#if CATCH
             try
             {
+#endif
+                bool createdNew;
+                mutex = new Mutex(true, uniqueMutexName, out createdNew);
+
+                if (!createdNew)
+                {
+                    const string windowTitle = "StarterForm"; // TODO: use class StarterForm.<window title>
+                    IntPtr hWnd = FindWindow(null, windowTitle);
+
+                    if (hWnd != IntPtr.Zero)
+                        SetForegroundWindow(hWnd);
+                    else
+                        MessageBox.Show("The application is already running, but the window could not be found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
                 GuiHelper.setSelfDir();
                 Application.Run(new StarterForm());
+#if CATCH
             }
             catch (Exception ex)
             {
-                MessageBox.Show("General Error:\r\n" + ex.Message + "\r\nHRESULT: " + ex.HResult.ToString() + "\r\nSource: " + ex.Source + "\r\nStack:\r\n" + ex.StackTrace);
+                MessageBox.Show($"General Error:\r\n{ex.Message}\r\nHRESULT: {ex.HResult}\r\nSource: {ex.Source}\r\nStack:\r\n{ex.StackTrace}");
             }
+#endif
         }
     }
 }
